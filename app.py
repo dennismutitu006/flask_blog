e767c7a83a6536b3dbe990be3f9938126b10e524
dennismutitu <dennismutitu06@gmail.com>
2024-08-15 11:42:31 +0300

    attempting to configure an orm SQLALchemy

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 #from config import Config
+from posts.blueprint import posts
 
 app = Flask(__name__)
 if __name__ == "__main__":
     app.run(debug=True)
+
+app.register_blueprint(posts, url_prefix='/blog')
+
+
+db = SQLAlchemy(app)
